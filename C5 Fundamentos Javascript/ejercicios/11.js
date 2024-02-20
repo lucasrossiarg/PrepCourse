000a34ce89d57641d8d4563fcf97e4e63f3be653
lucasrossiarg <lucas7588@hotmail.com>
2024-02-20 19:30:52 -0300

    M5 completo

--- C5 Fundamentos Javascript/ejercicios/11.js
+++ C5 Fundamentos Javascript/ejercicios/11.js
@@ -5,6 +5,13 @@ function esMayorDeEdad(fechaNacimiento) {
   // Devuelve true si lo es, caso contrario, retorna false.
   // PISTA: Investiga el método getFullYear() de la clase Date para obtener el año actual.
   // Tu código:
+
+  var fecha = fechaNacimiento.getFullYear()
+  var fechaDeHoy = new Date().getFullYear()
+
+  if (fechaDeHoy - fecha > 18) return true;
+  else return false;
+
 }
 
 module.exports = esMayorDeEdad;
--- C5 Fundamentos Javascript/ejercicios/31.js
+++ C5 Fundamentos Javascript/ejercicios/31.js
@@ -1,6 +1,9 @@
 function areaDelTriangulo(base, altura) {
   // Calcula el área de un triángulo y retorna el resultado.
   // Tu código:
+
+  return (base * altura) / 2;
+
 }
 
 module.exports = areaDelTriangulo;
